3f5942c40178b3a1e942281335430de813ed8014
Gaia Iturralde <138412962+Gitrua07@users.noreply.github.com>
2025-04-10 15:40:55 -0700

    Implemented boilerplate comments and functions

---
+++ main.py
@@ -0,0 +1,16 @@
+"""This program sets a time and message. It triggers a desktop notification once the goal time is reached.
+
+Here lies the brief description of the program. Will expand on this later.
+"""
+
+def get_time():
+
+def get_message():
+
+def get_reminder():
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
