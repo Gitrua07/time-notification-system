b37ffc15a9cc02e5387de670be3f138474f68b80
Gaia Iturralde <138412962+Gitrua07@users.noreply.github.com>
2025-04-10 15:43:47 -0700

    Set up doc strings for functions

--- main.py
+++ main.py
@@ -2,12 +2,23 @@
 
 Here lies the brief description of the program. Will expand on this later.
 """
+def set_reminder():
+    """Sets the reminder values, time and message."""
+
+def set_message():
+    """Sets up the message string value."""
+
+def set_time():
+    """Sets the goal time for the notification."""
 
 def get_time():
+    """Gets the time that the notification will be triggered on."""
 
 def get_message():
+    """Gets the message for a specific goal time."""
 
 def get_reminder():
+    """Gets the message and time that a time will trigger."""
 
 def main():
     print("Hello World!")
